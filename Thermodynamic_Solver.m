52c0074b9cc82c64d70836a2b3f087b7d6f8f034
christiffervogts <christiffer.vogts221@gmail.com>
2026-06-10 12:17:00 -0400

    Initial Commit

---
+++ Thermodynamic_Solver.m
@@ -0,0 +1,19 @@
+function [pressure_out, temprature_out, volume_out, mass_out,...
+    spesific_volume_out, quality_out,internal_energy_out, enthlepy_out,...
+    entropy_out, work_out, heat_transfer_out] = Thermodynamic_Solver( ...
+    Material, Number_of_States, Pressure, Temprature, Volume, Mass, ...
+    Spesific_Volume, Quality, Internal_Energy, Enthlapy, Entropy, Work, Heat)
+%THERMODYNAMIC_SOLVER This is a solver for thermodynamic problems
+%This takes in whatever you have and spits out everything else
+    switch(Number_of_States)
+        case 1
+
+        case 2
+
+        case 3
+
+        case 4
+
+    end
+end
+
